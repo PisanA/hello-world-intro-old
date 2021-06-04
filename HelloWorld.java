b00d87eeecdfba29f6b5cd79741bbec20eff8ddf
margaret3991 <61333083+margaret3991@users.noreply.github.com>
2021-06-04 10:41:13 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,7 +13,7 @@ class HelloWorld {
         System.out.println("   ***");
         System.out.println("  *****");
         System.out.println(" *******");
-        System.out.println("********* ");
+        System.out.println("*********");
     }
     
     public static void printFace() {
